dc07648be703628b429525c10501849ae68db409
Azam Janah <m.hajimohamadjanah@bsp-shell.bn>
2022-12-01 13:49:47 +0800

    Added model training

---
+++ train_model.py
@@ -0,0 +1,109 @@
+from collections import Counter
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from sklearn.preprocessing import OneHotEncoder
+
+def fcn(n_classes):
+    model = keras.Sequential([
+        layers.Input(shape=(None, None, 3)),
+        layers.Conv2D(16, (3, 3), activation='relu'),
+        layers.MaxPool2D((3,3)),
+        layers.Conv2D(32, (3, 3), activation='relu'),
+        layers.MaxPool2D((3,3)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPool2D((3,3)),
+        layers.Conv2D(128, (7, 7), activation='relu'),
+        layers.Dropout(0.5),
+        layers.Conv2D(128, (1, 1), activation='relu'),
+        layers.Conv2D(n_classes, (1, 1), activation='sigmoid')
+    ])
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        loss=tf.keras.losses.BinaryCrossentropy()
+    )
+    return model
+
+def get_data(indir='./data/images/train/'):
+    labels = []
+    images = []
+    for label in os.listdir(indir):
+        subdir = os.path.join(indir, label)
+        for filename in os.listdir(subdir):
+            fullname = os.path.join(subdir, filename)
+            images.append(cv2.imread(fullname))
+            labels.append(label)
+
+    return np.array(images), np.array(labels).reshape(-1, 1)
+
+def train_test_index_split(X, y, ratio=0.2):
+    train_index = []
+    test_index = []
+    for ycur in np.unique(y[:, 0]):
+        index_match = np.argwhere(y.ravel() == ycur).flatten()
+        cutoff = int(index_match.shape[0] * ratio)
+        left, right = np.split(index_match, [cutoff])
+        test_index.append(left)
+        train_index.append(right)
+
+    train_index = np.concatenate(train_index)
+    test_index = np.concatenate(test_index)
+    
+    np.random.shuffle(train_index)
+    np.random.shuffle(test_index)
+
+    return train_index, test_index
+
+def multilabel_statistics(yact, ypred, labels):
+    n_labels = len(labels)
+    # actual, pred
+    obs = np.zeros(shape=(n_labels + 1, n_labels + 1))
+    for act, pred in zip(yact, ypred):
+        act_index = np.argwhere(act == 1).flatten()
+        likely = np.argwhere(pred >= 0.5).flatten() 
+        if likely.shape[0] == 0:
+            obs[act_index, n_labels] += 1
+        else:
+            for l in likely:
+                obs[act_index, l] += 1
+
+    print(obs)
+
+    return obs
+
+
+
+if __name__ == '__main__':
+    images, labels = get_data()
+
+    # Encode labels for model
+    le = OneHotEncoder(sparse=False).fit(labels)
+    unique_labels = le.categories_[0]
+    n_labels = len(unique_labels)
+
+    # Samples per categories
+    print(unique_labels, n_labels)
+
+    # Create train & test indices
+    train_index, test_index = train_test_index_split(images, labels, ratio=0.2)
+
+    # Prep data & label for training
+    Xtrain = images[train_index, :, :, :] / 255.
+    ytrain = le.transform(labels[train_index, :]).reshape(-1, 1, 1, n_labels)
+
+    model = fcn(n_labels)
+    history = model.fit(Xtrain, ytrain, epochs=10, batch_size=32)
+
+    # Prep data & label for testing
+    Xtest = images[test_index, :, :, :] / 255.
+    ytest = le.transform(labels[test_index, :])
+
+    # Make predictions
+    ypred = model.predict(Xtest).reshape(-1, n_labels)    
+
+    multilabel_statistics(ytest, ypred, unique_labels)
+
--- video2images.py
+++ video2images.py
@@ -1,46 +1,70 @@
 import cv2
+import os
 
-vidfile = 'data/videos/for_training.mp4'
-outdir = 'data/images/train'
-source = cv2.VideoCapture(vidfile)
+# Prepare frame labelling function
+def label_frame(frame_no):
+    if 1 <= frame_no <= 193:
+        return '1984'
+    if 222 <= frame_no <= 412:
+        return 'donkey'
+    if 448 <= frame_no <= 597:
+        return 'digital'
+    if 618 <= frame_no <= 824:
+        return 'rig'
+    if 860 <= frame_no <= 1048:
+        return 'monument'
+    return 'background'
 
-# Read first image from video feed
-success, image = source.read()
-frame_count = 0
-image_w, image_h, _ = image.shape
-image_min = min(image_w, image_h)
-
-while success:
-    # Rotate 180 degrees
-    image = cv2.rotate(image, cv2.ROTATE_180)
-    frame_count += 1
-    outfile = f'img_{frame_count:04d}.jpg'
-
-    # Center crop image
-    image_crop = image[
-        image_w // 2 - image_min // 2:image_w // 2 + image_min // 2,
-        image_h // 2 - image_min // 2:image_h // 2 + image_min // 2,
-        :
-    ]
-
-    # Resize to 224 x 224
-    image_small = cv2.resize(image_crop, (224, 224))
-
-    # Write image to disk
-    cv2.imwrite(outdir + '/' + outfile, image_small)
-    print (f'... writing {outfile}')
-    cv2.imshow('out', image_small)
+# main function
+if __name__ == '__main__':
+    vidfile = 'data/videos/for_training.mp4'
+    outdir = 'data/images/train'
+    source = cv2.VideoCapture(vidfile)
     
-    # Listen to keypress
-    keypress = cv2.waitKey(1000 // 60)
-    if keypress == 27: # Esc key
-        break
-    if keypress == 32: # Spacebar key
-        cv2.waitKey(0)
-
-    print (f'Reading frame {frame_count + 1}')
+    # Read first image from video feed
     success, image = source.read()
+    frame_count = 0
+    image_w, image_h, _ = image.shape
+    image_min = min(image_w, image_h)
+    
+    while success:
+        # Rotate 180 degrees
+        image = cv2.rotate(image, cv2.ROTATE_180)
+        frame_count += 1
+        outfile = f'img_{frame_count:04d}.jpg'
+    
+        # Center crop image
+        image_crop = image[
+            image_w // 2 - image_min // 2:image_w // 2 + image_min // 2,
+            image_h // 2 - image_min // 2:image_h // 2 + image_min // 2,
+            :
+        ]
+    
+        # Resize to 224 x 224
+        image_small = cv2.resize(image_crop, (224, 224))
+    
+        # label current frame
+        label = label_frame(frame_count)
+        subdir = outdir + '/' + label
 
-
-print('Done')
-cv2.destroyAllWindows()
+        # create directory if not exist
+        os.makedirs(outdir + '/' + label) if not os.path.exists(subdir) else None
+    
+        # Write image to disk
+        cv2.imwrite(subdir + '/' + outfile, image_small)
+        print (f'... writing {outfile}')
+        cv2.imshow('out', image_small)
+        
+        # Listen to keypress
+        keypress = cv2.waitKey(1000 // 60)
+        if keypress == 27: # Esc key
+            break
+        if keypress == 32: # Spacebar key
+            cv2.waitKey(0)
+    
+        print (f'Reading frame {frame_count + 1}')
+        success, image = source.read()
+    
+    
+    print('Done')
+    cv2.destroyAllWindows()
